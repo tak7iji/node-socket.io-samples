3a0cc63f9a450c078e3e3028c281817a8ca188a3
root <root@yhansv4021.(none)>
2012-11-06 14:10:00 +0900

    add data store sample

---
+++ store/app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket) {
+  socket.on('set data', function(data) {
+    socket.set('data', data);
+    console.log(socket);
+    // callbackをつけても良い
+    //socket.set('data', data, function() {
+    //  socket.emit('data set');
+    //});
+  });
+  socket.on('client send', function(msg) {
+    socket.get('data', function(err, data) {
+      socket.emit('server send', data+':'+msg);
+    });
+  });
+});
